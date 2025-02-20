768fa273572daf687bb14b20e03fba348e25bc75
Everest Paudel <paudeleverest09@gmail.com>
2025-02-20 10:33:23 +0545

    entity debug

--- src/main/java/com/offnine/carten/modal/Order.java
+++ src/main/java/com/offnine/carten/modal/Order.java
@@ -12,7 +12,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
-
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -36,6 +36,7 @@ import com.offnine.carten.domain.PaymentStatus;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
+@Table(name="orders")
 public class Order{
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
